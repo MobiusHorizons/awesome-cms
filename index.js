20b33ee99ac78c32d3460c0a720690e1fb987d8f
Paul Martin <paul.martin@cqlcorp.com>
2015-11-11 08:17:33 -0500

    static site

--- index.js
+++
@@ -1,8 +0,0 @@
-var fs    = require('fs'),
-	data  = require('./api'),
-	theme = require('./theme/index.js');
-
-theme.render(data, function(name, contents){
-	console.log('writing "' + name + '"');
-	fs.writeFile(name, contents, 'utf8');
-});
